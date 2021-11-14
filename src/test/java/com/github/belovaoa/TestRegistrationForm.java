0bf0a037dfc4f03383caf7a4f9b18231753d5a87
belova_oa <belovva.o@gmail.com>
2021-11-14 11:59:46 +0300

    add .gitignore

---
+++ src/test/java/com/github/belovaoa/TestRegistrationForm.java
@@ -0,0 +1,20 @@
+package com.github.belovaoa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestRegistrationForm {
+
+    @BeforeAll
+    static void ConfigAllTests() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void fillFormTest() {
+
+
+
+    }
+}
